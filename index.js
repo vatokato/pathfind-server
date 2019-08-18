1881a5f28f119078ecf339edc935ea37945d0645
RomanOt <vatokato@gmail.com>
2019-08-18 23:55:34 +0500

    fix

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
-  res.send('Path API. Use: POST http://localhost:3000/path (x1,y1,z1,x2,y2,z2)');
+  res.send('Path API. Use: POST http://localhost:3001/path (x1,y1,z1,x2,y2,z2)');
 });
 
 app.post('/path', pathModel.getDistance);
 
 app.listen(3001, function() {
-  console.log('Path API started');
+  console.log('Path API started. http://localhost:3001');
 });
