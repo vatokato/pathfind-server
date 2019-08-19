7977aea3248d945bda5db72c0284b6c5adda36da
RomanOt <vatokato@gmail.com>
2019-08-19 20:50:21 +0500

    fixes

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.get('/', function(req, res) {
   res.send('Path API. Use: POST http://localhost:3001/path (x1,y1,z1,x2,y2,z2)');
 });
--- models/path.js
+++ models/path.js
@@ -1,17 +1,21 @@
 exports.getDistance = function(req, res) {
+  let name1 = req.body.name1 || '';
+  let name2 = req.body.name2 || '';
   let path = {
-    x1: req.body.x1 || 0,
-    y1: req.body.y1 || 0,
-    z1: req.body.z1 || 0,
+    x1: parseFloat(req.body.x1) || 0,
+    y1: parseFloat(req.body.y1) || 0,
+    z1: parseFloat(req.body.z1) || 0,
 
-    x2: req.body.x2 || 0,
-    y2: req.body.y2 || 0,
-    z2: req.body.z2 || 0
+    name2: req.body.name2 || '',
+    x2: parseFloat(req.body.x2) || 0,
+    y2: parseFloat(req.body.y2) || 0,
+    z2: parseFloat(req.body.z2) || 0
   };
 
   let scale = Math.abs(req.body.scale) || 1;
 
   let distance = scale*Math.sqrt((Math.pow(path.x2-path.x1,2)+Math.pow(path.y2-path.y1,2)+Math.pow(path.z2-path.z1,2) ));
 
-  res.send({distance:distance});
+  res.send({distance:distance, name1, name2});
+
 }
